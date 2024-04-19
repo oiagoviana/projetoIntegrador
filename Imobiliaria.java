868685f2c235bcc4228c8dc9916bad4096d83ca3
Vinicius Fideles <163475559+ViniciusAlb21@users.noreply.github.com>
2024-04-18 22:25:24 -0300

    atualizado

--- Imobiliaria.java
+++ Imobiliaria.java
@@ -1,146 +1,105 @@
-import java.util.Scanner;
+public class Imobiliaria {
 
-public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
-    System.out.println("Por favor, responda algumas perguntas para encontrar a casa mais adequada para você.");
-
-    System.out.println("Em que local você gostaria de morar? ");
-    System.out.println("A-Centro");
-    System.out.println("B-Subúrbio");
-    System.out.println("C-Periferia");
-    String localizacao = scanner.nextLine();
-    
-    System.out.println("Deseja uma casa próxima a uma estação de metrô (s/n)?");
-    char respMetro = scanner.next().charAt(0);
-    boolean metro = (respMetro == 's' || respMetro == 'S'); 
-
-    System.out.println("Quantos quartos você precisa? ");
-    int numQuartos = scanner.nextInt();
-    
-    System.out.print("Você precisa de garagem (s/n)? ");
-    char respostaGaragem = scanner.next().charAt(0);
-    boolean temGaragem = (respostaGaragem == 's' || respostaGaragem == 'S');
-
-    System.out.println("Qual é o valor máximo que você pode pagar pela casa? ");
-    double valorMaximo = scanner.nextDouble();
-    scanner.nextLine();
-
-    // Verificando as preferências do comprador
-    //Centro casa 1 
-    if (localizacao.equalsIgnoreCase("A") && metro && numQuartos == 1 && temGaragem && valorMaximo >= 400000) {
-        System.out.println("Parabéns! Temos uma ótima opções para você no centro da cidade.");
-        System.out.println("Temos uma casa com 1 quarto, 2 banheiros e 1 vaga para carro localizada em Centro, São Paulo-SP, no valor de R$ 400.000!");
-        System.out.println("Deseja prosseguir (s/n)? ");
-
-        String Resposta = scanner.nextLine(); //le a Resposta do usuario
-
-         if (Resposta.equalsIgnoreCase("s")) {
-            System.out.println("Ótimo! Por favor conclua seu cadastro para darmos processedência a compra!");
-
-            System.out.println("Informe seu nome completo:");
-            String nome = scanner.nextLine();
-
-            System.out.println("Informe seu cpf:");
-            String cpf = scanner.nextLine();
-
-            System.out.println("Infome seu telefone:");
-            String tel = scanner.nextLine();
-
-            System.out.println("Informe seu e-mail:");
-            String email = scanner.nextLine();
-
-            System.out.println("Obrigado! Entraremos em contato para agentar uma visita ao imóvel!");
-
-        } else if (Resposta.equals("n")) {
-            System.out.println("Entendido! Obrigado por sua resposta.");
-        } else {
-            System.out.println("Desculpe, não entendi sua resposta.");
-        }
-
-
-    //Centro casa 2     
-    } else if (localizacao.equalsIgnoreCase("A") && metro && numQuartos == 2 && temGaragem && valorMaximo >= 500000) {
-        System.out.println("Parabéns! Temos uma ótima opções para você no centro da cidade.");
-        System.out.println("Temos uma casa com 2 quartos, 2 banheiros e 2 vagas para carro localizado em Centro, São Paulo-SP, no valor de R$ 500.000! ");
-        System.out.println("Deseja prosseguir (s/n)? ");
-
-        String Resposta = scanner.nextLine(); //le a Resposta do usuario
-
-         if (Resposta.equalsIgnoreCase("s")) {
-            System.out.println("Ótimo! Por favor conclua seu cadastro para darmos processedência a compra!");
-
-            System.out.println("Informe seu nome completo:");
-            String nome = scanner.nextLine();
-
-            System.out.println("Informe seu cpf:");
-            String cpf = scanner.nextLine();
-
-            System.out.println("Infome seu telefone:");
-            String tel = scanner.nextLine();
-
-            System.out.println("Informe seu e-mail:");
-            String email = scanner.nextLine();
-
-            System.out.println("Obrigado! Entraremos em contato para agentar uma visita ao imóvel!");
-
-        } else if (Resposta.equals("n")) {
-            System.out.println("Entendido! Obrigado por sua resposta.");
-        } else {
-            System.out.println("Desculpe, não entendi sua resposta.");
-        }
-
-    //Centro casa 3
-    } else if (localizacao.equalsIgnoreCase("A") && metro && numQuartos == 3 && temGaragem && valorMaximo >= 600000) {
-        System.out.println("Parabéns! Temos uma ótima opções para você no centro da cidade.");
-        System.out.println("Temos uma casa com 3 quartos, 2 banheiros e 2 vagas para carro localizado em Republica, São Paulo-SP, no valor de R$ 600.000! ");
-        System.out.println("Deseja prosseguir (s/n)? ");
-
-        String Resposta = scanner.nextLine(); //le a Resposta do usuario
-
-         if (Resposta.equalsIgnoreCase("s")) {
-            System.out.println("Ótimo! Por favor conclua seu cadastro para darmos processedência a compra!");
-
-            System.out.println("Informe seu nome completo:");
-            String nome = scanner.nextLine();
-
-            System.out.println("Informe seu cpf:");
-            String cpf = scanner.nextLine();
-
-            System.out.println("Infome seu telefone:");
-            String tel = scanner.nextLine();
-
-            System.out.println("Informe seu e-mail:");
-            String email = scanner.nextLine();
-
-            System.out.println("Obrigado! Entraremos em contato para agentar uma visita ao imóvel!");
-
-        } else if (Resposta.equals("n")) {
-            System.out.println("Entendido! Obrigado por sua resposta.");
-        } else {
-            System.out.println("Desculpe, não entendi sua resposta.");
-        }
-
-    
-        
-    } else if (localizacao.equalsIgnoreCase("B") && metro && numQuartos >= 1 && temGaragem && valorMaximo >= 200000) {
-        System.out.println("Encontramos uma bela casa no subúrbio que pode atender às suas necessidades.");
-
-
-
-
-
-
-
-
-
-
-    } else if (localizacao.equalsIgnoreCase("C") && numQuartos >= 1 && valorMaximo >= 100000) {
-        System.out.println("Há uma casa aconchegante na periferia que pode se encaixar no seu orçamento.");
-    } else {
-        System.out.println("Desculpe, não encontramos nenhuma casa que atenda às suas preferências.");
+        menuPrincipal();
     }
 
-    scanner.close();
-}
+    private static void menuPrincipal() {
+        while (true) {
+            System.out.println();
+            System.out.println("Escolha uma opção:");
+            System.out.println("A - Cadastrar veículo");
+            System.out.println("B - Listar veículos");
+            System.out.println("A - Cadastrar Imovel");
+            System.out.println("B - Listar Casas");
+            System.out.println("B - Listar Apartamentos");
+            System.out.println("B - Listar Terrenos");
+            System.out.println("X - Sair");
+            System.out.println();
+            System.out.print("Digite a sua escolha: ");
+            var escolha = LerDados.lerTexto().toUpperCase();
+            System.out.println();
+
+            if (escolha.equals("A")) {
+                cadastrarVeiculo();
+            } else if (escolha.equals("B")) {
+                listarVeiculos();
+            } else if (escolha.equals("C")) {
+                buscarVeiculo();
+            } else if (escolha.equals("D")) {
+                manutencao();
+            } else if (escolha.equals("E")) {
+                vendaDePecas();
+            } else if (escolha.equals("F")) {
+                limpeza();
+            } else if (escolha.equals("G")) {
+                pintura();
+                cadastrarImovel();
+            } else if (escolha.equals("X")) {
+                return;
+            } else {
+                System.out.println("Esta não é uma opção válida. Vamos tentar novamente!");
+            }
+        }
+    }
+
+    private static void cadastrarImovel() {
+        System.out.println("Informe o tipo de imóvel (Casa, Apartamento ou Terreno)");
+        var tipoImovel = LerDados.lerTexto();
+
+        // DADOS CASA
+        if (tipoImovel.equals("Casa")) {
+            System.out.println("Informe o local da casa");
+            var localizacaoCasa = LerDados.lerTexto();
+
+            System.out.println("Informe a quantidade de comodos da casa");
+            var comodosCasa = LerDados.lerTexto();
+
+            System.out.println("Tem garagem?(sim/não)");
+            var garagemCasa = LerDados.lerTexto().toUpperCase();
+            if (garagemCasa.equals("SIM")) {
+                System.out.println("Quantas vagas?");
+                var vagasCasa = LerDados.lerInt("");
+            }
+
+            System.out.println("Informe o valor da casa");
+            var valorCasa = LerDados.lerInt("");
+
+        }
+        // DADOS APARTAMENTO
+        if (tipoImovel.equals("Apartamento")) {
+            System.out.println("Informe o local do apartamento");
+            var localizacaoApartamento = LerDados.lerTexto();
+
+            System.out.print("Informe quantos comodos tem o apartamento");
+            var comodosApartamento = LerDados.lerInt("");
+
+            System.out.println("Informe o andar do apartamento");
+            var andarApartamento = LerDados.lerInt("");
+
+            System.out.println("Tem garagem?(sim/não)");
+            var garagemApartamento = LerDados.lerTexto().toUpperCase();
+            if (garagemApartamento.equals("SIM")) {
+                System.out.println("Quantas vagas?");
+                var vagasApartamento = LerDados.lerInt("");
+            }
+
+            System.out.println("Informe o valor do apartamento");
+            var valorApartamento = LerDados.lerInt("");
+
+        }
+
+        // DADOS APARTAMENTO
+        if (tipoImovel.equals("Terreno")) {
+            System.out.println("Informe o local do terreno");
+            var localizacaoTerreno = LerDados.lerTexto();
+
+            System.out.println("Informe quantos mêtros quadrados o terreno tem");
+            var metrosTerreno = LerDados.lerTexto();
+
+            System.out.println(("Valor do terreno"));
+            var valorTerreno = LerDados.lerInt("");
+        }
+
+    }
 }
