852d3798fd0c8e998d00d5cf08656745c6701d76
ti_menor <ti_menor@VINFOR03.local.chriscintos.com.br>
2024-05-08 14:27:52 -0300

    ...

--- alugarImovel.java
+++ alugarImovel.java
@@ -1,4 +1,23 @@
-public class alugarImovel {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.chrono.IsoChronology;
+import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+	
+	
+	public record alugarImovel (
+	        int codigo,
+	        String nomePeca,
+	        String nomeComprador,
+	        int quantidade,
+	        LocalDateTime hora,
+	        BigDecimal preco
+	) {
     
     public static void main(String[] args) {
         alugarImovel();
---
+++ arquivoAlugar.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class arquivoAlugar {
+
+	    private static final String ARQUIVO = "alugar.txt";
+
+	    /*public static List<alugarImovel> lerTudo() {
+	        var listona = Arquivo.lerLinhas(ARQUIVO);
+	        return alugarImovel.construirTodos(listona);
+	    }*/
+	    
+	
+
+}
