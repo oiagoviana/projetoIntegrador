77d90f33a442f4932291d1486b690b8ddf737dd1
Ranielly Evellyn <2004ninacunha@gmail.com>
2024-06-01 23:31:08 -0300

    Agendar

--- comprarImovel.java
+++ comprarImovel.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class comprarImovel {
 
@@ -7,6 +10,7 @@ public class comprarImovel {
             System.out.println("1 - Listar Casa");
             System.out.println("2 - Listar Apartamento");
             System.out.println("3 - Listar Terreno");
+            System.out.println("4 - Agendar Visita");
             System.out.println("X - VOLTAR");
             var escolha = LerDados.lerTexto().toUpperCase();
 
@@ -16,7 +20,9 @@ public class comprarImovel {
                 //listarApartamentoCompra();
             } else if(escolha.equals("3")){
                 //listarTerrenoCompra;
-            } else if(escolha.equals("X")){
+            } else if (escolha.equals("4")) {
+                agendarVisita();
+            }else if(escolha.equals("X")){
                 return;
             }
         }
@@ -66,9 +72,32 @@ public class comprarImovel {
         System.out.println("Situação Venda: " + p.situacaoImovel());
 	}
     
+    private static void agendarVisita() {
+        System.out.println("Digite o endereço do imóvel a ser agendado:");
+        var endereco = LerDados.lerTexto().toUpperCase();
+        var propriedades = ArquivoCadastrar.lerTudo();
+        var propriedade = Propriedade.buscar(propriedades, endereco);
+        if (propriedade.isEmpty()) {
+            System.out.println("Imóvel não encontrado.");
+            return;
+        }
 
+        System.out.println("Digite a data de agendamento (dd/mm/aaaaa):");
+        var dataStr = LerDados.lerTexto();
+        System.out.println("Digite o horário de agendamento (HH:mm):");
+        var horaStr = LerDados.lerTexto();
 
-
-
+        try {
+            LocalDate data = LocalDate.parse(dataStr);
+            LocalTime hora = LocalTime.parse(horaStr);
+            LocalDateTime dataHora = LocalDateTime.of(data, hora);
+            System.out.println("Visita agendada para: " + dataHora);
+        } catch (Exception e) {
+            System.out.println("Data ou horário inválido. Tente novamente.");
+        }
+    }
+        
 
 }
+
+
